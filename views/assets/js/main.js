b7ac962b61ee8851645d494bc33cbe252203dd09
Trey <treyw1850@gmail.com>
2018-08-01 02:50:00 -0400

    added carousel

--- views/assets/js/main.js
+++ views/assets/js/main.js
@@ -1,1 +1,14 @@
 console.log('hey beautiful')
+
+  $(document).ready(function(){
+    $('.carousel').carousel({
+        shift: 30,
+        
+    });
+  });
+
+
+//   $('.carousel.carousel-slider').carousel({
+//     fullWidth: true
+//   });
+        
